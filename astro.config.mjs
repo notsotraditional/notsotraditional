134c7416205310625f392f4358031a303cd46ce2
Ruben de Villiers <ruben@notsotraditional.com>
2024-08-16 16:19:32 +0200

    Added SEO improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
+  site:'https://notsotraditional.com',
+  integrations: [svelte(), tailwind(), sitemap()],
   output: 'hybrid',
   adapter: vercel()
 });
